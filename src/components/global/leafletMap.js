fea7fc39021e12cbca8c7b43c9d739075fb83df0
Toni Halmetoja <36439969+ToniHalmetoja@users.noreply.github.com>
2022-03-16 13:29:15 +0100

    country data added, countries highlighted on hover

--- src/components/global/leafletMap.js
+++ src/components/global/leafletMap.js
@@ -1,6 +1,22 @@
 import { useCallback, useEffect, useMemo, useState } from 'react'
-import { MapContainer, TileLayer } from 'react-leaflet'
+import { MapContainer, TileLayer, L, GeoJSON } from 'react-leaflet'
+import countries from "../data/countries.json"
+import "./mapStyles.css"
+
 const mapboxLink = 'https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoidG9uaWhhbG1ldG9qYSIsImEiOiJjbDBzN2NqYWQwMXNxM2JtaDZ3OGthYWQ1In0.ALPmMcLVezZiSLWzY3sdFA';
+const attribution = `
+Map data & copy
+<a href='https://www.openstreetmap.org/'>
+  OpenStreetMap
+  </a>
+contributors,
+  <a href='https://creativecommons.org/licenses/by-sa/2.0/'>
+    CC-BY-SA
+  </a>
+  , Imagery ©
+<a href='https://www.mapbox.com/'>
+  Mapbox
+  </a>`
 
 const center = [30, -0]
 const zoom = 2.4
@@ -32,17 +48,75 @@ export function DisplayPosition({ map }) {
 }
 
 export function DisplayMap() {
+
+  const countryStyle = {
+    weight:1,
+  };
+
+  const onEachCountry = (country, layer) => {
+    const countryName = country.properties.ADMIN;
+    layer.bindPopup(countryName)
+    layer.on('mouseover', function () {
+      this.setStyle({
+        'fillColor': '#ffffff'
+      });
+    });
+    layer.on('mouseout', function () {
+      console.log("beep")
+      this.setStyle({
+        'fillColor': '#3388ff'
+      });
+    });
+  }
+
+  function highlightFeature(e) {
+    this.setStyle({
+    weight: 5,
+    color: '#666',
+    dashArray: '',
+    fillOpacity: 0.7
+    });
+   }
+
+ 
+  console.log(countries)
+      function countryColor(d) {
+        return d > 1000000 ? '#016c59' :
+            d > 100000 ? '#1c9099' :
+            d > 10000 ? '#67a9cf' :
+            d > 1000 ? '#bdc9e1' :
+            d > 100 ? '#f6eff7' :
+            'white';
+    }
+
+    function style(feature) {
+        return {
+            weight: 2,
+            opacity: 1,
+            color: 'white',
+            dashArray: '3',
+            fillOpacity: 0.5,
+            fillColor: countryColor(feature.properties.wineConsumption)
+        };
+    }
+
+
+    // const borderLayer = L.geoJson(countryData, {
+    //   style: style,
+    // });
+
   const [map, setMap] = useState(null)
 
   const displayMap = useMemo(
     () => (
       <MapContainer style={{ height: "85vh", width: "70vw"}} center={center} zoom={zoom} whenCreated={setMap} zoomSnap="0.2">
-        <TileLayer
-            attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        {/* <TileLayer
+            attribution = {attribution}
             maxZoom="18"
             id="mapbox.light"
             url={mapboxLink}
-        />
+        /> */}
+        <GeoJSON style={countryStyle} data={countries.features} onEachFeature={onEachCountry}/>
       </MapContainer>
     ),
     [],
